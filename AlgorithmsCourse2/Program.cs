473b8e2ec09bb7337374c6bc304e4b47ad04a656
tolikcode <tolikcode@gmail.com>
2013-09-17 00:22:13 +0300

    Algorithm for minimizing the weighted sum of jobs completion times

--- AlgorithmsCourse2/Program.cs
+++ AlgorithmsCourse2/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using AlgorithmsCourse2.TasksImplementations;
 
 namespace AlgorithmsCourse2
 {
@@ -9,6 +11,56 @@ namespace AlgorithmsCourse2
     {
         static void Main(string[] args)
         {
+            ScheduleJobs();
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
         }
+
+        /// <summary>
+        /// Greedy algorithms from lecture for minimizing the weighted sum of completion times of a set of jobs.
+        /// 
+        /// File format:
+        /// [number_of_jobs]
+        /// [job_1_weight] [job_1_length]
+        /// [job_2_weight] [job_2_length]
+        /// </summary>
+        public static void ScheduleJobs()
+        {
+            string[] taskLines = File.ReadAllLines(@"TasksData\jobs.txt");
+            List<Job> jobs = new List<Job>();
+
+            for (int i = 1; i < taskLines.Length; i++)
+            {
+                string[] lineValues = taskLines[i].Split(' ');
+                jobs.Add(new Job(int.Parse(lineValues[0]), int.Parse(lineValues[1])));
+            }
+            
+            Console.WriteLine("Initial ordering completion times sum: " + CalcWeightedCompletionTimesSum(jobs));
+
+            List<Job> differeceSortedJobs = new List<Job>(jobs);
+            differeceSortedJobs.Sort(new DifferenceWeightJobComparer());
+            differeceSortedJobs.Reverse();
+            Console.WriteLine("Completion times sum of jobs ordered by difference W(i) - L(i): " + CalcWeightedCompletionTimesSum(differeceSortedJobs));
+
+            List<Job> ratioSortedJobs = new List<Job>(jobs);
+            ratioSortedJobs.Sort((x, y) => x.RatioCoefficient.CompareTo(y.RatioCoefficient));
+            ratioSortedJobs.Reverse();
+            Console.WriteLine("Completion times sum of jobs ordered by ratio W(i)/L(i): " + CalcWeightedCompletionTimesSum(ratioSortedJobs));
+        }
+
+        private static long CalcWeightedCompletionTimesSum(IEnumerable<Job> jobs)
+        {
+            long weightedCompletionTimesSum = 0, 
+                currentCompletionTime = 0;
+            foreach (Job job in jobs)
+            {
+                currentCompletionTime += job.Length;
+                weightedCompletionTimesSum += currentCompletionTime*job.Weight;
+            }
+
+            return weightedCompletionTimesSum;
+        }
+
     }
 }
---
+++ AlgorithmsCourse2/TasksImplementations/Job.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmsCourse2.TasksImplementations
+{
+    internal struct Job
+    {
+        private readonly int weight;
+        private readonly int length;
+
+        public Job(int weight, int length)
+        {
+            this.weight = weight;
+            this.length = length;
+        }
+
+        public int Weight                        // Importance(priority) of a job
+        {
+            get { return weight; }
+        }
+
+        public int Length                        // Time required to do a job
+        {
+            get { return length; }
+        }  
+        
+        public int DifferenceCoefficient         // Jobs with bigger coefficient must be done earilier
+        {
+            get { return weight - length; }
+        }
+
+        public double RatioCoefficient              // Jobs with bigger coefficient must be done earilier
+        {
+            get { return (double)weight/(double)length; }
+        }
+    }
+
+    /// <summary>
+    /// Compares by DifferenceCoefficient and then by Weight for equal DifferenceCoefficients
+    /// </summary> 
+    internal class DifferenceWeightJobComparer : IComparer<Job>
+    {
+        public int Compare(Job x, Job y)
+        {
+            return x.DifferenceCoefficient == y.DifferenceCoefficient 
+                ? x.Weight.CompareTo(y.Weight) 
+                : x.DifferenceCoefficient.CompareTo(y.DifferenceCoefficient);
+        }
+    }
+}
